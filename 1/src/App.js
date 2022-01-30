ed65d21f1a1e4749d6384b4392db67df03110894
Максим Шевцов <shievtsovm14@gmail.com>
2022-01-30 15:43:56 +0300

    first commit

---
+++ 1/src/App.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import moment from 'moment'
+import './index.css'
+
+
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+function DateTimePretty(Component){
+    return function (props){
+        let now = moment(new Date());
+        let duration = moment.duration(now.diff(props.date));
+        let hours = duration.asHours()
+        let date
+        if(hours<1)
+            date = `${duration.asMinutes().toFixed(0)} минут назад`
+        else if (hours<24)
+            date = `${hours.toFixed(0)} часов назад`
+        else
+            date = `${duration.asDays().toFixed(0)} дней назад`
+        return <Component {...props} date={date}/>
+    }
+}
+
+const DateNewFormat = DateTimePretty(DateTime)
+
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateNewFormat date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
